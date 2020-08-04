0336a0a2d6af408a70ec58cab5c6703bc1a1d6e3
ObidulKadir <rezwankadirobaidul@gmail.com>
2020-08-04 23:25:01 +0600

    first commit

---
+++ DTR for80.py
@@ -0,0 +1,83 @@
+# import the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+# import dataset
+dataset = pd.read_csv('D://work/for thesis/new alogorithm try/80 data/80Updrs/Book1.csv')
+X = dataset.iloc[:,[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22]].values
+y = dataset.iloc[:,22].values
+
+# split into training and testing dataset
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state=0) # 25% for testing dataset
+
+# re-scale feature values
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test) 
+
+# fit the decision tree (classification) to the training dataset
+from sklearn.tree import DecisionTreeClassifier
+classifier = DecisionTreeClassifier(criterion = 'entropy', random_state=0)
+classifier.fit(X_train, y_train)
+
+
+# predict
+y_pred = classifier.predict(X_test)
+#accuracy of our model
+'''c=0
+for i in range(0,len(y_pred)):
+    if(y_pred[i]==y_test[i]):
+        c=c+1
+accuracy=c/len(y_pred)
+print("Accuracy is")
+print(accuracy)
+'''
+# confusion matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+# visualize training result
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_train, y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+Xpred = np.array([X1.ravel(), X2.ravel()] + [np.repeat(0, X1.ravel().size) for _ in range(21)]).T
+# Xpred now has a grid for x1 and x2 and average value (0) for x3 through x13
+pred = classifier.predict(Xpred).reshape(X1.shape)   # is a matrix of 0's and 1's !
+plt.contourf(X1, X2, pred,
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c=ListedColormap(("red", "green"))(i), label = j)
+plt.title("Decision Tree (Training set)")
+plt.xlabel('Symptoms')
+plt.ylabel('UPDRS-iii')
+plt.legend()
+plt.show()
+
+# visualize test result
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_train, y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+Xpred = np.array([X1.ravel(), X2.ravel()] + [np.repeat(0, X1.ravel().size) for _ in range(21)]).T
+# Xpred now has a grid for x1 and x2 and average value (0) for x3 through x13
+pred = classifier.predict(Xpred).reshape(X1.shape)   # is a matrix of 0's and 1's !
+plt.contourf(X1, X2, pred,
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c=ListedColormap(("red", "green"))(i), label = j)
+plt.title("Decision Tree (Test set)")
+plt.xlabel('Symptoms')
+plt.ylabel('UPDRS-iii')
+plt.legend()
+plt.show()
---
+++ SVMduraion.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 30 23:43:39 2019
+
+@author: kadir-CSE
+"""
